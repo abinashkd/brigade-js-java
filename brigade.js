0ada1dc37a17d60cdc33d7c9ce6147df98d6f125
Abinash <abidalai@in.ibm.com>
2020-10-05 14:35:45 +0530

    .

--- brigade.js
+++ brigade.js
@@ -35,8 +35,8 @@ events.on("test-done", (e, project) => {
   dockerBuild.tasks = [
     "dockerd-entrypoint.sh &",
     "sleep 20",
-	"cd /src"
-    "docker build -t abinashkd/brigade-java-test:latest Dockerfile",
+	"cd /src",
+    "docker build -t abinashkd/brigade-java-test:latest .",
     "docker login -u $DOCKER_USER -p $DOCKER_PASS",
     "docker push abinashkd/brigade-java-test:latest"
   ]
