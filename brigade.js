123dbbdbdb156332a82be258302d248161712ed9
Abinash <abidalai@in.ibm.com>
2020-10-05 18:03:34 +0530

    .

--- brigade.js
+++ brigade.js
@@ -24,8 +24,6 @@ events.on("test-done", (e, project) => {
 
   dockerBuild.image = "docker:dind"
   dockerBuild.privileged = true;
-  dockerBuild.lxc-conf = "lxc.aa_profile=unconfined";
-
   
   dockerBuild.env = {
     DOCKER_DRIVER: "overlay"
