3eb9345ca428ed4b13480aab5dfcc06966034876
Abinash <abidalai@in.ibm.com>
2020-10-08 14:22:33 +0530

    Promethous Config

--- src/main/java/com/example/brigade/WebController.java
+++ src/main/java/com/example/brigade/WebController.java
@@ -1,5 +1,8 @@
 package com.example.brigade;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -9,6 +12,14 @@ public class WebController {
     @RequestMapping("/")
 	public @ResponseBody String greeting() {
 		return "Hello, World";
+	}
+
+	@PostMapping("/student")
+	public @ResponseBody String student(@RequestBody String name){
+
+
+		return "student "+ name +" successful";
+		
 	}
     
 }
