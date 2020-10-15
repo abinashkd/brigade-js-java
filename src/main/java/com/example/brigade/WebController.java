7409353bac37d3c81a34793fcc05eb1e00f7bd6f
Abinash <abidalai@in.ibm.com>
2020-10-15 10:17:18 +0530

    ErrorStack+package.json

--- src/main/java/com/example/brigade/WebController.java
+++ src/main/java/com/example/brigade/WebController.java
@@ -1,6 +1,11 @@
 package com.example.brigade;
 
+import java.io.FileInputStream;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +26,22 @@ public class WebController {
 		return "student "+ name +" successful";
 		
 	}
+
+	@GetMapping("/dbassertion")
+  public ResponseEntity<String> asser() throws Exception {
+    // logger.info("Uid {}",uid);
+
+    try {
+      new FileInputStream("D:/abc.txt").close();
+      ;
+    } catch (Exception e) {
+
+      e.printStackTrace();
+      throw e;
+    }
+
+    return new ResponseEntity<String>(String.format("UUID Notfound"), HttpStatus.NOT_FOUND);
+
+  }
     
 }
